d33c5f3d325c411449e03cf6b6cde3f4e5cb945b
artem <mnastkin@gmail.com>
2025-04-15 14:25:33 +0300

    Первый коммит

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src='/Kz4Y1l7qNnA.jpg' alt='Example' className="App-logo" />
+                <h1>Извините, основной сайт по техническим причинам не работает.</h1>
+                <a href='/path/to/your/file.pdf' download className="App-link">
+                    Скачать результаты финала(PDF)
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
