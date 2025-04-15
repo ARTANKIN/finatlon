546ecd5f8678d1c8f1745e8b41e83ce44a9402ab
artem <mnastkin@gmail.com>
2025-04-15 15:09:38 +0300

    1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
             <header className="App-header">
                 <img src='/Kz4Y1l7qNnA.jpg' alt='Example' className="App-logo" />
                 <h1>Извините, основной сайт по техническим причинам не работает.</h1>
-                <a href='/path/to/your/file.pdf' download className="App-link">
-                    Скачать результаты финала(PDF)
-                </a>
+                {/*<a href='/path/to/your/file.pdf' download className="App-link">*/}
+                {/*    Скачать результаты финала(PDF)*/}
+                {/*</a>*/}
             </header>
         </div>
     );
