a95aa2a9f60d11bef02e5081cca3b9b870e03eea
artem <mnastkin@gmail.com>
2025-04-17 14:38:28 +0300

    1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src='/Kz4Y1l7qNnA.jpg' alt='Example'/>
+                <img src='/fin.jpg' alt='Example'/>
                 <h1>Извините, основной сайт по техническим причинам не работает.</h1>
-                <a href='/Лауреаты_финатлон_форума.pdf' download className="App-link">
-                    Скачать результаты финала(PDF)
-                </a>
             </header>
         </div>
     );
